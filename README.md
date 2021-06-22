eefc8d7f735a6eca7de0bf28f8d894d7f2444e30
Elie Mugenzi <eliemugenzi@gmail.com>
2021-06-22 14:41:35 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+`rwanda-phone-utils`
---
+++ src/constants/index.ts
@@ -0,0 +1,18 @@
+export default {
+  prefix: "250",
+  shortLength: 9,
+  fullLength: 12,
+  format: "(+250) 7XX XXX XXX",
+  short: "7XXXXXXXX",
+  dashed: "07X-XXX-XXXX",
+  telcos: [
+    { value: "78", label: "MTN" },
+    { value: "79", label: "MTN" },
+    { value: "72", label: "Airtel" },
+    { value: "73", label: "Airtel" },
+  ],
+  errors: {
+    short: "Phone number is too short",
+    invalid: "Phone number is invalid",
+  },
+};
